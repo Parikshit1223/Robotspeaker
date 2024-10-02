bac14fb98a1c38b114b38a27d218cec07507c05d
Parikshit Singh <parikshitsingh1223@gmail.com>
2024-10-02 23:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     while True :
         x = input("Enter what do you want me to speak :")
         if x == "q":
-            os.system("say'bye bye friend'")
+            os.system("say 'bye bye friend'")
             break
         command = f"say {x}"
         os.system(command)
